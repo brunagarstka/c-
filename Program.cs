4ddcb1f38a388c578dfa73d1ad492ada74bd6a84
Brunagarstka <bruna.luisa.garstkas2@gmail.com>
2024-08-08 15:58:15 -0300

    Sobre C#

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using c_.Models;
+Pessoa p1 = new Pessoa();
+p1.Nome = "Bruna";
+p1.Idade = 17;
+p1.Apresentar();
+/*
+Exempo de conversação de string para int:
+String a = "15";
+int b = 0;
+int.TryParse(a, out b);
+Console.WriteLine(b);*/

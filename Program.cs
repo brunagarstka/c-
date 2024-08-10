3f5ec934d9b5dbfd1a23d4c3c017e6d1a1d649f8
Brunagarstka <bruna.luisa.garstkas2@gmail.com>
2024-08-09 21:02:23 -0300

    subindo02

--- Program.cs
+++ Program.cs
@@ -1,5 +1,107 @@
-﻿using c_.Models;
-Calculadora calc = new Calculadora();
+﻿using System.Data.SqlTypes;
+using c_.Models;
+
+string opcao;
+bool exibirMenu = true;
+while(exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+
+         case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+         case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+           // Environment.Exit(0);
+            break;
+        
+        default:
+            Console.WriteLine("Opção inválida");
+    }
+}
+Console.WriteLine("O programa se encerrou");
+
+
+
+
+
+
+
+
+
+
+/*int soma = 0, numero = 0;
+do
+{
+    Console.WriteLine("Digite um número: (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+} while(numero != 0);
+
+Console.WriteLine($"Total da soma dos numeros digitados é {soma}");
+*/
+
+
+
+
+
+
+
+/*
+int numero = 5;
+int contador = 0;
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador}  Execução:  {numero} x {contador} = {numero * contador}");
+    contador++;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+/*int numero = 5;
+
+for(int contador = 0; contador <= 10; contador++)
+{
+   Console.WriteLine($"{numero} x {contador} = {numero * contador}"); 
+}
+*/
+
+
+
+
+
+
+
+
+/*Calculadora calc = new Calculadora();
 
 int numeroIncremento = 10;
 Console.WriteLine(numeroIncremento);
@@ -10,7 +112,10 @@ int numeroDecremento = 20;
 Console.WriteLine("Decrementando o 20");
 numeroDecremento++;
 
-Console.WriteLine(numeroDecremento);
+Console.WriteLine(numeroDecremento);*/
+
+
+
 
 
 
@@ -28,10 +133,27 @@ calc.Tangente(30);*/
 
 
 
+
+
+
+
+
+
+
+
 /*Pessoa p1 = new Pessoa();
 p1.Nome = "Bruna";
 p1.Idade = 17;
 p1.Apresentar();*/
+
+
+
+
+
+
+
+
+
 /*
 Exempo de conversação de string para int:
 String a = "15";
--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484b272cddcccd57eef930d0ddad9d7b4caa2d11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae244640ef80de8640e6ee68d2ccc07f181b62b")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
